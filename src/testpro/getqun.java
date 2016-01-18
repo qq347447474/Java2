0b49087898a4e684c7302f05b51982d4284b13f5
qq347447474 <347447474@qq.com>
2016-01-18 14:00:29 +0800

    'fsdsdffds'

--- src/testpro/getqun.java
+++ src/testpro/getqun.java
@@ -21,6 +21,7 @@ public class getqun {
     public static String sendPost() {
         PrintWriter out = null;
         
+        
         BufferedReader in = null;
         String result = "";
         try {
